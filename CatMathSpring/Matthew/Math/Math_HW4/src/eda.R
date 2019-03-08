7dd94d396796bccbfe73c8f04b8cda3a0b868592
Matthew Vanaman <m_vanaman@icloud.com>
2019-03-08 15:01:17 -0500

    deleted shit

--- CatMathSpring/Matthew/Math/Math_HW4/src/eda.R
+++
@@ -1,9 +0,0 @@
-setwd("~/Documents/Class/CatMathSpring/Matthew/Math/Math_HW4")
-library('ProjectTemplate')
-load.project()
-
-for (dataset in project.info$data)
-{
-  message(paste('Showing top 5 rows of', dataset))
-  print(head(get(dataset)))
-}
